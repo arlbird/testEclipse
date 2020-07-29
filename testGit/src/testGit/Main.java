de27ea9941b53fc27280dbb6a713707dd65787ff
arlbird <suzukiwork7@yahoo.co.jp>
2020-07-29 10:20:17 +0900

    二回目

--- testGit/src/testGit/Main.java
+++ testGit/src/testGit/Main.java
@@ -3,6 +3,7 @@ package testGit;
 public class Main {
 	public static void main(String[] args) {
 		System.out.println("穴を掘るなら天を衝く、墓穴掘っても掘り続け、掘りぬけたなら、俺の勝ち！");
+		System.out.println("俺はシモンだ、カミナの兄貴じゃない！俺は俺だ『穴掘りシモン』だ！！");
 	}
 
 }
